0902a4cad999c51fce2747f5780dde5400395d60
cwj <282330332@qq.com>
2025-04-09 17:38:31 +0800

    Compose初步提交

---
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.composedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android", true)
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+//@Composable
+//fun GreetingPreview() {
+//    ComposeDemoTheme {
+//        Greeting("Android")
+//    }
+//}
+
+
+@Composable
+fun Greeting(name: String, isShowName: Boolean) {
+    val showName = if (isShowName) "显示名字" else "不显示"
+    Text(text = "Hello $name!  $showName")
+}
+
+@Preview(name = "测试", widthDp = 100, heightDp = 200, showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Zhujiang", false)
+}
